fcee3386d14bf22e6d8801d84fa446428ec26885
Johannes Koch <johanneskoch@fico.com>
2024-09-22 22:45:32 +0200

    feat(Update with feedback from PR)

--- .projenrc.ts
+++ .projenrc.ts
@@ -66,9 +66,6 @@ const project = new cdk.JsiiProject({
 
 project.addTask('local-push', { exec: 'npx yalc push' }).prependSpawn(project.buildTask);
 
-project.tasks.tryFind('docgen')?.reset('echo no-execute on local');
-
-
 project.gitpod?.addCustomTask({
   init: 'npm ci',
   command: 'npx projen build',
