17753724d43140e8df7d1b0a5a3b994682a7d5c5
Josh Begin <joshbegin@ipipeline.com>
2026-01-12 10:08:46 -0700

    Move code around to prevent unshift

--- src/steps/aws-assume-role.step.ts
+++ src/steps/aws-assume-role.step.ts
@@ -1,7 +1,12 @@
 import { Project } from 'projen';
 import { JobPermission, JobStep } from 'projen/lib/github/workflows-model';
-import { BashStepConfig, CodeCatalystStepConfig, GithubStepConfig, GitlabStepConfig, PipelineStep } from './step';
-
+import {
+  BashStepConfig,
+  CodeCatalystStepConfig,
+  GithubStepConfig,
+  GitlabStepConfig,
+  PipelineStep,
+} from './step';
 
 /**
  * Configuration for an AWS AssumeRoleStep
@@ -23,16 +28,20 @@ export interface AwsAssumeRoleStepConfig {
  * A step that assumes a role in AWS
  */
 export class AwsAssumeRoleStep extends PipelineStep {
-
-  constructor(project: Project, private readonly config: AwsAssumeRoleStepConfig) {
+  constructor(
+    project: Project,
+    private readonly config: AwsAssumeRoleStepConfig,
+  ) {
     super(project);
   }
 
   public toGitlab(): GitlabStepConfig {
-    const sessionName = this.config.sessionName ?? 'GitLabRunner-\${CI_PROJECT_ID}-\${CI_PIPELINE_ID}}';
+    const sessionName =
+      this.config.sessionName ??
+      'GitLabRunner-${CI_PROJECT_ID}-${CI_PIPELINE_ID}}';
     return {
       env: {
-        ...this.config.region ? { AWS_REGION: this.config.region } : {},
+        ...(this.config.region ? { AWS_REGION: this.config.region } : {}),
       },
       commands: [
         `export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" $(aws sts assume-role-with-web-identity --role-arn "${this.config.roleArn}" --role-session-name "${sessionName}" --web-identity-token \${AWS_TOKEN} --duration-seconds 3600 --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text))`,
@@ -43,32 +52,37 @@ export class AwsAssumeRoleStep extends PipelineStep {
   }
 
   public toGithub(): GithubStepConfig {
-    let steps: JobStep[] = [
-      {
-        name: 'AWS Credentials',
+    let steps: JobStep[] = [];
+
+    if (this.config.jumpRoleArn) {
+      steps.push({
+        name: 'Assume Jump Role',
         uses: 'aws-actions/configure-aws-credentials@v5',
         with: {
-          'role-to-assume': this.config.roleArn,
+          'role-to-assume': this.config.jumpRoleArn,
           'role-session-name': this.config.sessionName ?? 'GitHubAction',
-          ...this.config.region ? { 'aws-region': this.config.region } : { 'aws-region': 'us-east-1' },
+          ...(this.config.region
+            ? { 'aws-region': this.config.region }
+            : { 'aws-region': 'us-east-1' }),
+          'role-skip-session-tagging': true,
         },
+      });
+    }
+    steps.push({
+      name: 'AWS Credentials',
+      uses: 'aws-actions/configure-aws-credentials@v5',
+      with: {
+        'role-to-assume': this.config.roleArn,
+        'role-session-name': this.config.sessionName ?? 'GitHubAction',
+        ...(this.config.region
+          ? { 'aws-region': this.config.region }
+          : { 'aws-region': 'us-east-1' }),
       },
-    ];
+    });
     if (this.config.jumpRoleArn) {
-      // Add role chaining options to the first step (which will become the second step after unshift)
-      steps[0]?.with && (steps[0].with['role-chaining'] = true);
-      steps[0]?.with && (steps[0].with['role-skip-session-tagging'] = true);
-      steps.unshift(
-        {
-          name: 'Assume Jump Role',
-          uses: 'aws-actions/configure-aws-credentials@v5',
-          with: {
-            'role-to-assume': this.config.jumpRoleArn,
-            'role-session-name': this.config.sessionName ?? 'GitHubAction',
-            ...this.config.region ? { 'aws-region': this.config.region } : { 'aws-region': 'us-east-1' },
-            'role-skip-session-tagging': true,
-          },
-        });
+      // Add role chaining options to the second step
+      steps[1]?.with && (steps[1].with['role-chaining'] = true);
+      steps[1]?.with && (steps[1].with['role-skip-session-tagging'] = true);
     }
     return {
       steps,
@@ -83,7 +97,9 @@ export class AwsAssumeRoleStep extends PipelineStep {
   public toBash(): BashStepConfig {
     return {
       commands: [
-        `echo "Login to AWS using role ${this.config.roleArn} for region ${this.config.region ?? 'undefined'}"`,
+        `echo "Login to AWS using role ${this.config.roleArn} for region ${
+          this.config.region ?? 'undefined'
+        }"`,
       ],
     };
   }
@@ -96,5 +112,4 @@ export class AwsAssumeRoleStep extends PipelineStep {
       needs: [],
     };
   }
-
 }
